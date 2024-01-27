4bba2a2a2d8323c69bad247dc2b1c859ad068e88
Muhammad Hamza <91649627+MuhammadHamza00@users.noreply.github.com>
2024-01-27 23:51:47 +0500

    First Commit

---
+++ 151_reverse_words_of_string/sol.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "hello" << endl;
+    cout << "hello" << endl;
+    cout << "hello" << endl;
+    cout << "hello" << endl;
+    cout << "hello" << endl;
+    int hel = 9;
+	cin >> hel ;
+    return 0;
+}
