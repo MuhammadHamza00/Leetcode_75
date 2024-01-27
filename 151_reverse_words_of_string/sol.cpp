7c752084738e725f322f363b164393d59d73d9f3
Muhammad Hamza <91649627+MuhammadHamza00@users.noreply.github.com>
2024-01-27 23:52:22 +0500

    Update sol.cpp

--- 151_reverse_words_of_string/sol.cpp
+++ 151_reverse_words_of_string/sol.cpp
@@ -1,16 +1,63 @@
-#include <iostream>
-#include <string.h>
+class Solution {
+public:
+    string reverseWords(string s) {
+        string out = "";
+        if(s.empty()){
+            return "";
+        }
+        else if(s == " "){
+            return "";
+        }else{
+            int start = 0;
+            int end = s.length()-1;
 
-using namespace std;
+            if(s[0] == ' ' || s[end] == ' '){
 
-int main()
-{
-    cout << "hello" << endl;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
-    cout << "hello" << endl;
-    int hel = 9;
-	cin >> hel ;
-    return 0;
-}
+      
+            // trim the string
+            for(int i=0;i<s.length();i++){
+                if(s[i] != ' '){
+                    start = i;
+                    break;
+                }
+            }
+             for(int i=s.length()-1;i>=0;i--){
+                if(s[i] != ' '){
+                    end = i;
+                    break;
+                }
+            }
+            }
+                bool is_first = true;
+                bool is_space = true;
+                string container = "";
+
+                for(int i = end;i >= start; i--){
+                    if(s[i] == ' ' && is_space){
+                        if(is_first){
+                            out = out + container;
+                            is_first = false;
+                        }
+                        else{
+                            out = out + ' ' + container;
+                        }
+                        container = "";
+                        is_space = false;
+                    }
+                    if(s[i] != ' '){
+                        container = s[i] + container;
+                        is_space = true;
+                    }
+                }
+                if(out == ""){
+                    out = container;
+                }else{
+                    out = out + ' ' + container;
+                }
+                
+
+            }
+            return out;
+        }
+
+};
